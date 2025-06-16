d16fbd85881721d561e20042b0561940c308d218
JayXie <tsum950210@gmail.com>
2025-06-16 22:34:22 +0800

    feat: Add multiple global middlewares
    - Added CORS.
    
    - Added data compression middleware.
    
    - Added error handler.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,26 @@
 import("./utils/envValidator.js");
 
 import express from "express";
+import cors from "cors";
+import compression from "compression";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/authRoute";
 import { logger } from "./utils/logger";
 import { initPostgres, initRedis } from "./utils/database";
+import type { NextFunction, Request, Response } from "express";
 
 const app = express();
 const port = process.env["PORT"] ?? 3000;
 
+app.use(cors({ credentials: true }));
+app.use(compression());
 app.use(cookieParser(process.env["COOKIE_SECRET"]));
 app.use("/auth", authRoute);
+app.use("/files", filesRoute);
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+	logger.error(err);
+	res.status(500).json({ message: "Unknown error occurred" });
+});
 
 app.listen(port, async () => {
 	await initPostgres();
